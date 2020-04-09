a5c3f89357ea5181cd60586ebf84920d50eaa153
wangzongru <wang.zong@husky.neu.edu>
2020-04-09 13:27:22 -0400

    server.js confi changes

--- server.js
+++ server.js
@@ -16,9 +16,9 @@ const app = express()
 // mongoose.connect('mongodb://localhost:27017/whiteboard-cs4500-sp20',
 //     { useNewUrlParser: true, useUnifiedTopology: true })
 
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
     res.header("Access-Control-Allow-Origin",
-        "http://localhost:4200");
+        "*");
     res.header("Access-Control-Allow-Headers",
         "Origin, X-Requested-With, Content-Type, Accept");
     res.header("Access-Control-Allow-Methods",
