88fd7d3840aa1a144a9ee290c315d6f3e90fb8df
wangzongru <wang.zong@husky.neu.edu>
2020-04-09 13:41:59 -0400

    server.js confi changes

--- server.js
+++ server.js
@@ -1,37 +1,19 @@
 const express = require('express')
 const app = express()
 
-// var session = require('express-session')
-// app.use(session({
-//     resave: false,
-//     saveUninitialized: true,
-//     secret: 'any string'
-// }));
-//
-// var bodyParser = require('body-parser')
-// app.use(bodyParser.urlencoded({ extended: false }))
-// app.use(bodyParser.json())
-//
-// const mongoose = require('mongoose')
-// mongoose.connect('mongodb://localhost:27017/whiteboard-cs4500-sp20',
-//     { useNewUrlParser: true, useUnifiedTopology: true })
 
 app.use(function (req, res, next) {
-    res.header("Access-Control-Allow-Origin",
-        "*");
-    res.header("Access-Control-Allow-Headers",
-        "Origin, X-Requested-With, Content-Type, Accept");
-    res.header("Access-Control-Allow-Methods",
-        "GET, POST, PUT, DELETE, OPTIONS");
-    res.header("Access-Control-Allow-Credentials", "true");
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers',
+        'Content-Type, X-Requested-With, Origin');
+    res.header('Access-Control-Allow-Methods',
+        'GET, POST, PUT, PATCH, DELETE, OPTIONS');
     next();
 });
 
+
 require('./controllers/quizzes.controller.server')(app)
 require('./controllers/questions.controller.server')(app)
-//require('./controllers/users.controller.server')(app)
 
-// app.get('/hello', (req, res) =>
-//     res.send('Hello World!'))
 
-app.listen(process.env.PORT || 3000)
+app.listen(3000)
